1a5cdd5ec4e4ee30fcbcc3b531c8534ed4a671c3
BrunoMotaBr <87215947+BrunoMotaBr@users.noreply.github.com>
2023-01-08 20:34:03 -0300

    Add files via upload
    Sem funcionalidades nos botões apenas layout responsivo.

---
+++ js/main.js
@@ -0,0 +1,253 @@
+const posts = [
+    {
+        foto: "../img/imagem_perfil/img1.jpg",
+        nome: "Adriana Jonas",
+        Texto: "  ",
+        TemImagem: true,
+        imagem: "../img/imagem_perfil/img1.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img2.jpg",
+        nome: "Jonas Paulino",
+        Texto: "Adoro presepio!",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img1.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img3.jpg",
+        nome: "Ana Kathi",
+        Texto: "Cuide de quem corre do seu lado, e quem te quer bem essa é a coisa mais pura.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img4.jpg",
+        nome: "Bruna Sandra",
+        Texto: "Velas novas que comprei.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img2.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img5.jpg",
+        nome: "Jonas Rodrigues",
+        Texto: "Meu gatinho Mel.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img3.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img6.jpg",
+        nome: "Natalia Alves",
+        Texto: "Palmeiras, Palmeiras, Palmeiras, Palmeiras, Palmeiras, Palmeiras, Palmeiras!!!",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img7.jpg",
+        nome: "Katia Aderson",
+        Texto: "Partiu cinema.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img8.jpg",
+        nome: "Dougles da Silva",
+        Texto: "Lindo e forte",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img4.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img9.jpg",
+        nome: "Andrea Soares",
+        Texto: "  ",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img5.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img10.jpg",
+        nome: "Caroline Sanches",
+        Texto: "Meu marido nas alturas!!",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img6.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img11.jpg",
+        nome: "Alice Mota",
+        Texto: "Nada disso importa, sejemos felizes.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img12.jpg",
+        nome: "Kaio Silva",
+        Texto: "Adoro as borboletas.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img7.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img13.jpg",
+        nome: "Geovane Castro",
+        Texto: "Bora pra academiaa.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img14.jpg",
+        nome: "Marcos Mistars",
+        Texto: "Vida selvagem.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img8.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img15.jpg",
+        nome: "Ricardo Pereira",
+        Texto: "Logo logo fico ricoo.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img16.jpg",
+        nome: "Nicolas Santos",
+        Texto: "Comprei para minha filha.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img9.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img17.jpg",
+        nome: "Renan Alves",
+        Texto: "Lontras são incriveis.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img10.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img18.jpg",
+        nome: "Jerry Smith",
+        Texto: "Plutão é um planeta.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img19.jpg",
+        nome: "Elizabete Silva",
+        Texto: "Melhores ferias da minha vida.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img11.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img20.jpg",
+        nome: "Jackson Andre",
+        Texto: "Friiioooo.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img12.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img21.jpg",
+        nome: "Izabela Miranda",
+        Texto: "Espero que todos estejam bem. E que não falte comida ná casa de ninguem",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img22.jpg",
+        nome: "Anne Silva",
+        Texto: "'Garcinha' essa garça rsrsrs.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img13.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img23.jpg",
+        nome: "Allan Rodrigues",
+        Texto: "Meu cachorrinho.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img14.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img24.jpg",
+        nome: "Anderson Silva",
+        Texto: "Minha Gatinha Louis.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img15.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img25.jpg",
+        nome: "Fernanda Pereira",
+        Texto: "Feliz Natal!!! E um prospero ano novooo a todos.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img26.jpg",
+        nome: "Eliana Pereira",
+        Texto: "Presentes de natal.",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img16.jpg"
+    },
+    {
+        foto: "../img/imagem_perfil/img27.jpg",
+        nome: "Michele Sanches",
+        Texto: "Trabalhando até tarde hoje.",
+        TemImagem: false,
+        imagem: ""
+    },
+    {
+        foto: "../img/imagem_perfil/img28.jpg",
+        nome: "Guilerme Oliveira",
+        Texto: "Feliz Natal!!!",
+        TemImagem: true,
+        imagem: "../img/imgs_Postis/img17.jpg"
+    }
+];
+
+const quantidadeDeCurtidas = document.getElementById('quantidade-curtida');
+const post = document.getElementById('post');
+
+exibirNaTela();
+
+function exibirNaTela(){
+    posts.forEach(postagem => {
+        if(postagem.TemImagem){
+            post.innerHTML += `
+            <section class="postagens-conteudo">
+                <div class="perfil">
+                    <img class="img_perfil" src="${postagem.foto}">
+                    <h2 class="nome_perfil">${postagem.nome}</h2>
+                </div>
+                <div class="postagem">
+                    <div class="postagem-texto">
+                        <p>
+                            ${postagem.Texto}
+                        </p>
+                    </div>
+                    <img class="postagem_imagem" src=${postagem.imagem}>
+                </div>
+                <div class="botoes">
+                    <button class="botao botao-curtir">Curtir</button>
+                    <button class="botao botao-comentar">Cometar</button>
+                    <button class="botao botao-compartilhar">Compartilhar</button>
+                </div>
+            </section>`
+        }else{
+            post.innerHTML += `
+            <section class="postagens-conteudo">
+                <div class="perfil">
+                    <img class="img_perfil" src="${postagem.foto}">
+                    <h2 class="nome_perfil">${postagem.nome}</h2>
+                </div>
+                <div class="postagem">
+                    <div class="postagem-texto">
+                        <p>
+                            ${postagem.Texto}
+                        </p>
+                    </div>
+                    
+                </div>
+                <div class="botoes">
+                    <button class="botao botao-curtir">Curtir</button>
+                    <button class="botao botao-comentar">Cometar</button>
+                    <button class="botao botao-compartilhar">Compartilhar</button>
+                </div>
+            </section>`
+        }
+        
+    })
+    
+}
